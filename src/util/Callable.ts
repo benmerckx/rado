df46c7a7b7c7dde2d64150f1e020b14bd33f8c29
Ben Merckx <ben@codeurs.be>
2023-02-13 13:31:04 +0100

    Mention sources

--- src/util/Callable.ts
+++ src/util/Callable.ts
@@ -1,3 +1,6 @@
+// This is based on https://github.com/WebReflection/custom-function/blob/31fd7271c86354c1b2f80618c2124ee143827e98/esm/index.js
+// but we don't actually want the Function prototype so it's not included
+
 const {setPrototypeOf} = Object
 
 export class Callable {
