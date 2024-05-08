f6b240e603cddb0405fc1230d5895d623e2ceae5
Ben Merckx <ben@codeurs.be>
2024-05-08 12:08:25 +0200

    Interim

--- test/driver/better-sqlite3.test.ts
+++ test/driver/better-sqlite3.test.ts
@@ -1,7 +1,7 @@
 import {Runtime} from '@sinclair/carbon'
 import {testDriver} from '../TestDriver.ts'
 
-if (!Runtime.isBun())
+if (Runtime.isNode())
   await testDriver('better-sqlite3', async () => {
     const {default: Database} = await import('better-sqlite3')
     const {connect} = await import('../../src/driver/better-sqlite3.ts')
