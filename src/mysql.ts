235c30cf7552e73de17631a4af6076ab31e5981d
Ben Merckx <ben@codeurs.be>
2024-08-06 11:21:52 +0200

    Interim

---
+++ src/mysql.ts
@@ -0,0 +1,4 @@
+export * from './mysql/MysqlColumns.ts'
+export * from './mysql/MysqlDialect.ts'
+export * from './mysql/MysqlTable.ts'
+
--- src/postgres.ts
+++ src/postgres.ts
@@ -1,2 +1,3 @@
 export * from './postgres/PostgresColumns.ts'
 export * from './postgres/PostgresDialect.ts'
+export * from './postgres/PostgresTable.ts'
