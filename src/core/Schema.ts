226cf3f60c16f94e276560128ac5293a178f39e7
Ben Merckx <ben@codeurs.be>
2024-08-08 09:40:17 +0200

    Fix JSR "slow" types

--- src/core/Schema.ts
+++ src/core/Schema.ts
@@ -1,21 +1,30 @@
 import {
-  table,
   type Table,
   type TableConfig,
-  type TableDefinition
+  type TableDefinition,
+  table
 } from './Table.ts'
 
-export function schema<SchemaName extends string>(schemaName: SchemaName) {
-  type Prefix<TableName extends string> = `${SchemaName}.${TableName}`
-  return {
-    table<Definition extends TableDefinition, TableName extends string>(
-      tableName: TableName,
-      columns: Definition,
-      config?: (self: Table<Definition>) => TableConfig<Prefix<TableName>>
-    ) {
-      return <Table<Definition, Prefix<TableName>>>(
-        table(tableName, columns, config, schemaName)
-      )
+type Prefix<
+  SchemaName extends string,
+  TableName extends string
+> = `${SchemaName}.${TableName}`
+export interface Schema<SchemaName extends string> {
+  table<Definition extends TableDefinition, TableName extends string>(
+    tableName: TableName,
+    columns: Definition,
+    config?: (
+      self: Table<Definition>
+    ) => TableConfig<Prefix<SchemaName, TableName>>
+  ): Table<Definition, Prefix<SchemaName, TableName>>
+}
+
+export function schema<SchemaName extends string>(
+  schemaName: SchemaName
+): Schema<SchemaName> {
+  return <Schema<SchemaName>>{
+    table(tableName, columns, config) {
+      return table(tableName, columns, config, schemaName)
     }
   }
 }
